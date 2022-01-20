7848cbf14a1bf10917371101a033beb526143c72
Marcin Pham <marcin.kamo@gmail.com>
2022-01-20 19:24:54 +0100

    added top level handling in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import com.company.entities.Orders;
+import com.company.handlers.ReportHandler;
+import com.company.handlers.iHandler;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,7 +13,7 @@ public class Main {
     private static SessionFactory sessionFactory = null;
 
     public static void main(String[] args) {
-        sessionFactory = getSessionFactory();
+        /*sessionFactory = getSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
 
@@ -20,6 +22,28 @@ public class Main {
 
         tx.commit();
         session.close();
+
+        */
+
+        iHandler handler;
+
+        switch(args[0]) {
+            case "-crud":
+                return;
+            case "-report":
+                handler = new ReportHandler();
+                break;
+            case "-order":
+                return;
+            default:
+                System.out.println("Must specify option");
+                return;
+        }
+
+        handler.handle(args);
+
+        return;
+
     }
     private static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
---
+++ src/com/company/handlers/iHandler.java
@@ -0,0 +1,7 @@
+package com.company.handlers;
+
+public interface iHandler {
+
+    public void handle(String[] args);
+
+}
